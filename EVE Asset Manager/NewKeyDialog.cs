b545583e60e765a66c7c3561b1b6579a47472a96
Will <Will@MARCO>
2008-01-22 17:47:01 -0500

    Added default sort by name; trimmed whitespace on NewKeyDialog properties.

--- EVE Asset Manager/NewKeyDialog.cs
+++ EVE Asset Manager/NewKeyDialog.cs
@@ -17,12 +17,12 @@ namespace HeavyDuck.Eve.AssetManager
 
         public int UserID
         {
-            get { return Convert.ToInt32(id_box.Text); }
+            get { return Convert.ToInt32(id_box.Text.Trim()); }
         }
 
         public string ApiKey
         {
-            get { return key_box.Text; }
+            get { return key_box.Text.Trim(); }
         }
     }
 }
